fd92277dad46f80983a8a0ff343fb800a47682f5
Anuththara Imanshi <anuththaraimanshi842@gmail.com>
2025-07-20 16:44:43 +0100

    add

--- food delivery/src/Components/Navbar/Navbar.jsx
+++ food delivery/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets';
 
 const Navbar = () => {
+  const[menu,setmenu]=useState("home");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="App Logo" className="logo" />
       <ul className="navbar-menu ">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li> 
-        <li>contact us</li>
+        <li onClick={()=>setmenu ("home")}className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setmenu ("menu")}className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setmenu ("mobile-app")}className={menu==="mobile-app"?"active":""}>mobile-app</li> 
+        <li onClick={()=>setmenu ("contact us")}className={menu==="contact us"?"active":""}>contact us</li>
         </ul>
       <div className="navbar-right">
         <img src={assets.search_icon } alt=""  />
