3fe5689fe9c338c5a4d74781875ef3db442bb496
Anuththara Imanshi <anuththaraimanshi842@gmail.com>
2025-07-26 11:52:27 +0100

    add

--- food delivery/src/Components/FoodDisplay/FoodDisplay.jsx
+++ food delivery/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,7 @@ const FoodDisplay = (category) => {
       <h2>Top dishes near you</h2>
       <div className='food-display-list'>
         {food_list.map((item, index) => {
-            return<FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.discription} image={item.image} />
+            return<FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image} />
         })}
 
       </div>
