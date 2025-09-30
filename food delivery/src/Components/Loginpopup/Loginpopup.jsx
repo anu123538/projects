bdf9e28bb17379c574b6a34790f655d0be80696f
Anuththara Imanshi <anuththaraimanshi842@gmail.com>
2025-09-30 16:07:06 +0100

    update

--- food delivery/src/Components/Loginpopup/Loginpopup.jsx
+++ food delivery/src/Components/Loginpopup/Loginpopup.jsx
@@ -4,6 +4,9 @@ import './Loginpopup.css'
 const Loginpopup = () => {
   return (
     <div className='login-popup'>
+        <form className='login-popup-container'>
+            <div className='login-popup-title'></div>
+        </form>
       
     </div>
   )
